b1353d9b122f6be33ee86ce0891a7ce30c02a789
Егор Федотов <egorfedotovarz@gmail.com>
2022-12-15 21:14:28 +0300

    correct mistakes

--- exceptions.py
+++ exceptions.py
@@ -7,26 +7,3 @@ class EmptyHomeworksList(Exception):
 class ResponseStatusError(Exception):
     '''Неожиданный код ответа API.'''
     pass
-
-
-class MessageNotSent(Exception):
-    '''сообщение не отправлено.'''
-    pass
-
-
-class ResponseStatusCode(Exception):
-    '''Ошибка запроса к API.'''
-    pass
-
-
-class InvalidRequestApi(Exception):
-    '''Некорректный статус запроса API.'''
-
-
-class CheckKeyHw(Exception):
-    '''Исключение неправильного формата ответа API.'''
-
-
-class UnknownHWStatusException(Exception):
-    """Unknown homework status exception."""
-    pass
